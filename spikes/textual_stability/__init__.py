228f58e4917fd7c98be2aaea0800819143a77a94
zhnt <zhnt@foxmail.com>
2026-05-31 07:51:33 +0000

    test: remove textual stability spike

--- spikes/textual_stability/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Textual stability spike package."""
-
-from spikes.textual_stability.config import SpikeConfig, SpikeProfile, load_profile
-
-__all__ = ["SpikeConfig", "SpikeProfile", "load_profile"]
