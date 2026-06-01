ff84d51feac9a24a418f4491d30dd3a5b38ef45d
zhnt <zhnt@foxmail.com>
2026-06-01 10:28:04 +0000

    feat: add work p0 core types
    Refs #28

---
+++ src/loushang/work/__init__.py
@@ -0,0 +1,15 @@
+from loushang.work.types import (
+    DeliveryHint,
+    WorkEvent,
+    WorkOperation,
+    WorkRun,
+    WorkRunStatus,
+)
+
+__all__ = [
+    "DeliveryHint",
+    "WorkEvent",
+    "WorkOperation",
+    "WorkRun",
+    "WorkRunStatus",
+]
