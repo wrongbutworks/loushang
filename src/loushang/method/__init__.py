433fe1597f408e14abec3d7ee2b4ec3d87410bb9
zhnt <zhnt@foxmail.com>
2026-06-02 10:45:25 +0000

    feat: add method p1 core types

---
+++ src/loushang/method/__init__.py
@@ -0,0 +1,15 @@
+from loushang.method.types import (
+    MethodContext,
+    MethodDescriptor,
+    MethodPlan,
+    MethodProjection,
+    MethodStep,
+)
+
+__all__ = [
+    "MethodContext",
+    "MethodDescriptor",
+    "MethodPlan",
+    "MethodProjection",
+    "MethodStep",
+]
